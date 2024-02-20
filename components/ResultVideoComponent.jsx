e12912bc6ff41c9038f0b452da5452ad2a72ab8d
MagnusDevelopr <sudhanshusharma103@gmail.com>
2024-02-20 17:53:43 +0530

    style: fix generate transcribe button

--- components/ResultVideoComponent.jsx
+++ components/ResultVideoComponent.jsx
@@ -5,7 +5,7 @@ import styles from '@styles/fileediting.module.css'
 const ResultVideoComponent = ({filename}) => {
   return (
     <div className={styles.videoContainer}>
-    <ReactPlayer url={`https://phrasify-uploads.s3.ap-south-1.amazonaws.com/${filename}`} controls={true} playsinline width={240} height='full'></ReactPlayer>
+    <ReactPlayer url={`https://phrasify-uploads.s3.ap-south-1.amazonaws.com/${filename}`} controls={true} playsinline width={260} height='full'></ReactPlayer>
     <div className={styles.buttonsDiv}>
     <button className={styles.captionGenerateButton}>
       <SparklesIcon className='h-7 w-7'/>
